54fd567d685473a3b9883ba150c8f3b93b32b962
Mikhail Koshel <mouseratti@gmail.com>
2018-11-25 22:19:17 +0600

    updated course

--- src/bas/1_start/first.py
+++ src/bas/1_start/first.py
@@ -1,2 +1,13 @@
+from variables import NAME, AGE, POSITION
 print("Hello, BAS!")
-print(__name__)
+
+TEXT = """
+My name is {}. I'm {} years old. It's about {} days.
+My position is {}.
+"""
+
+age_days = 365.0 * AGE 
+
+
+print(TEXT.format(NAME, AGE, age_days, POSITION))
+
