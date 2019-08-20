8399dfb2ebb57ea041142b05e2351deb35cb2ced
Mikhail Koshel <mouseratti@gmail.com>
2019-08-20 10:23:22 +0600

    updated strings

--- python3/02_types.py
+++ python3/02_types.py
@@ -103,6 +103,52 @@ a.capitalize()
 a.capitalize == a.capitalize()
 "conca" + 'tenation'  # operator + calls str.__add__ method under the hood
 
+####### Some string methods
+# Method	Description
+# Python String capitalize()	Converts first character to Capital Letter
+# Python String center()	Pads string with specified character
+# Python String casefold()	converts to casefolded strings
+# Python String count()	returns occurrences of substring in string
+# Python String endswith()	Checks if String Ends with the Specified Suffix
+# Python String expandtabs()	Replaces Tab character With Spaces
+# Python String encode()	returns encoded string of given string
+# Python String find()	Returns the index of first occurrence of substring
+# Python String format()	formats string into nicer output
+# Python String index()	Returns Index of Substring
+# Python String isalnum()	Checks Alphanumeric Character
+# Python String isalpha()	Checks if All Characters are Alphabets
+# Python String isdecimal()	Checks Decimal Characters
+# Python String isdigit()	Checks Digit Characters
+# Python String isidentifier()	Checks for Valid Identifier
+# Python String islower()	Checks if all Alphabets in a String are Lowercase
+# Python String isnumeric()	Checks Numeric Characters
+# Python String isprintable()	Checks Printable Character
+# Python String isspace()	Checks Whitespace Characters
+# Python String istitle()	Checks for Titlecased String
+# Python String isupper()	returns if all characters are uppercase characters
+# Python String join()	Returns a Concatenated String
+# Python String ljust()	returns left-justified string of given width
+# Python String rjust()	returns right-justified string of given width
+# Python String lower()	returns lowercased string
+# Python String upper()	returns uppercased string
+# Python String swapcase()	swap uppercase characters to lowercase; vice versa
+# Python String lstrip()	Removes Leading Characters
+# Python String rstrip()	Removes Trailing Characters
+# Python String strip()	Removes Both Leading and Trailing Characters
+# Python String partition()	Returns a Tuple
+# Python String maketrans()	returns a translation table
+# Python String rpartition()	Returns a Tuple
+# Python String translate()	returns mapped charactered string
+# Python String replace()	Replaces Substring Inside
+# Python String rfind()	Returns the Highest Index of Substring
+# Python String rindex()	Returns Highest Index of Substring
+# Python String split()	Splits String from Left
+# Python String rsplit()	Splits String From Right
+# Python String splitlines()	Splits String at Line Boundaries
+# Python String startswith()	Checks if String Starts with the Specified String
+# Python String title()	Returns a Title Cased String
+
+
 
 ##### string formatting
 "formatted {}".format('string')  # empty placeholder
